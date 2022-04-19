8bf865222b8e87fa31f6b9b98a5e5e3762e71a6c
Alex <amojarrot@gmail.com>
2022-04-18 18:24:32 -0600

    Integrated nexus-prisma plugin

--- api/graphql/Exchanges.ts
+++ api/graphql/Exchanges.ts
@@ -1,24 +1,24 @@
 import { objectType, queryField, list, nonNull, stringArg, mutationField } from 'nexus';
+import { Exchange } from 'nexus-prisma';
 
-export const Exchange = objectType({
-  name: 'Exchange',
-  description: 'This represents an exchange and its information.',
+export const ExchangeObject = objectType({
+  name: Exchange.$name,
+  description: Exchange.$description,
   definition(t) {
-    t.nonNull.int('id');
-    t.nonNull.string('name');
+    t.field(Exchange.id);
+    t.field(Exchange.name);
   },
 });
 
 export const ExchangeQuery = queryField('exchanges', {
-  // this looks funky
-  type: nonNull(list(nonNull(Exchange))),
+  type: nonNull(list(nonNull(ExchangeObject))),
   resolve: (_root, _args, ctx) => {
     return ctx.prisma.exchange.findMany();
   },
 });
 
 export const AddExchange = mutationField('addExchange', {
-  type: Exchange,
+  type: ExchangeObject,
   args: {
     name: nonNull(stringArg()),
   },
