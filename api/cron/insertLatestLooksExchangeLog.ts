d317f5b22856e3c5d995392c22a44f480c24f4cd
Alex <amojarrot@gmail.com>
2022-04-26 12:54:08 -0600

    date problem with the cron job is fixed I only need to refactor.

--- api/cron/insertLatestLooksExchangeLog.ts
+++ api/cron/insertLatestLooksExchangeLog.ts
@@ -1,15 +1,17 @@
 import { SimpleIntervalJob, AsyncTask } from 'toad-scheduler'
 import { PrismaClient } from '@prisma/client'
 import { getLastDayExchangeLog } from 'api/utils/TheGraph/LooksRare/getLastXLogs'
+import { roundDownUtcDate } from 'api/utils/Date/dateConverter'
 
 const prisma = new PrismaClient()
 
 const task = new AsyncTask(
   'Update DB for latest exchange information',
   async () => {
+    const todayISOString = new Date(roundDownUtcDate(new Date()) * 1000).toISOString()
     const looksLogDay = await getLastDayExchangeLog()
     const lastLooksLog = await prisma.exchangeLog.findFirst({
-      where: { date: '2022-04-23' },
+      where: { date: todayISOString },
     })
     await prisma.exchange.update({
       where: {
@@ -22,13 +24,13 @@ const task = new AsyncTask(
               id: lastLooksLog?.id || '',
             },
             create: {
-              date: '2022-04-23',
+              date: todayISOString,
               dailyTransactions: looksLogDay.dailyTransactions,
               dailyUsers: looksLogDay.dailyUsers,
               dailyVolume: looksLogDay.dailyVolumeExcludingZeroFee,
             },
             update: {
-              date: '2022-04-23',
+              date: todayISOString,
               dailyTransactions: looksLogDay.dailyTransactions,
               dailyUsers: looksLogDay.dailyUsers,
               dailyVolume: looksLogDay.dailyVolumeExcludingZeroFee,
@@ -44,7 +46,7 @@ const task = new AsyncTask(
 )
 
 export const job1 = new SimpleIntervalJob(
-  { seconds: 60, runImmediately: true },
+  { seconds: 15, runImmediately: true },
   task,
   'id_1'
 )
